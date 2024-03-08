3aa14064f229333aa9505d28be6acbb779aa368d
somnath142002 <stunnersomu14@gmail.com>
2024-03-08 09:27:45 +0530

    removed extension

--- src/Store.js
+++ src/Store.js
@@ -1,13 +1,13 @@
 import { createStore,combineReducers } from "redux"
 import UserDetails from "./reducer"
-import { composeWithDevTools } from "redux-devtools-extension";
+
 
 const store = createStore(
     combineReducers({
         userdetails:UserDetails,
 
     }),
-   composeWithDevTools()
+   
 )
 
 export default store;
